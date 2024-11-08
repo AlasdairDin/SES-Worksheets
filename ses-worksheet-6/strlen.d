f6b07c22af32c936eefad13b91bf5957a2473d88
alasdair <alasdair@SamsungQHDTV1002344.localdomain>
2024-11-08 14:00:38 +0000

    Uni Mass Upload

---
+++ ses-worksheet-6/strlen.d
@@ -0,0 +1,20 @@
+strlen.o: strlen.c ../../newlib-cygwin/newlib/libc/include/string.h \
+ ../../newlib-cygwin/newlib/libc/include/_ansi.h \
+ ../../newlib-cygwin/newlib/libc/include/newlib.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/config.h \
+ ../../newlib-cygwin/newlib/libc/include/machine/ieeefp.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/features.h \
+ ../../newlib-cygwin/newlib/libc/include/_newlib_version.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/reent.h \
+ ../../newlib-cygwin/newlib/libc/include/_ansi.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/_types.h \
+ ../../newlib-cygwin/newlib/libc/include/machine/_types.h \
+ ../../newlib-cygwin/newlib/libc/include/machine/_default_types.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/lock.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/cdefs.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/_locale.h \
+ ../../newlib-cygwin/newlib/libc/include/strings.h \
+ ../../newlib-cygwin/newlib/libc/include/sys/string.h \
+ ../../newlib-cygwin/newlib/libc/include/stdlib.h \
+ ../../newlib-cygwin/newlib/libc/include/machine/stdlib.h \
+ ../../newlib-cygwin/newlib/libc/include/alloca.h
